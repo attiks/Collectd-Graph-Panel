2449c542e5bead01185869af975ed36474e501bd
Peter <peter@attiks.com>
2011-10-09 20:21:32 +0200

    added host title + link to detail

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,10 @@ html_start();
 print '<div class="dashboard cols-' . $CONFIG['dashboard']['cols'] . '">';
   foreach ($CONFIG['dashboard']['graphs'] as $plugin) {
     print '<div class="dashboard-graph">';
-      printf('<img src="%s">'."\n", build_url('graph.php', $plugin));
+      printf('<h2><a href="%s">%s</a></h2>'."\n", $CONFIG['weburl'].'/host.php?h='.htmlentities($plugin['h']), $plugin['h']);
+      printf('<a href="%s">'."\n", build_url('detail.php', $plugin));
+        printf('<img src="%s">'."\n", build_url('graph.php', $plugin));
+      print '</a>';
     print '</div>';
   }
 print '</div>';
